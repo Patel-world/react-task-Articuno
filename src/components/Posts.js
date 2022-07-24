f01173077313b842fa05aa7a61f6b69f2e3780e6
patel <patel.dp2024@gmail.com>
2022-07-24 08:43:14 +0530

    make it better

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Button from './Button';
 
 const Posts = ({ posts, loading }) => {
   
@@ -28,7 +29,9 @@ const Posts = ({ posts, loading }) => {
           <div className="grid-item1"><p className="fw-bold">CONTACT</p><br></br>{item.contact_person}</div>
           <div className="grid-item1"><p className="fw-bold">CITY</p><br></br>{item.city}</div>
           <div className="grid-item1"><p className="fw-bold">STATE</p><br></br>{item.state}</div>
-          <div className="grid-item1"><button onClick={()=>{active=='view details' && isActive===item.uuid_ ? setActive('hide details'):setActive('view details');isActive===item.uuid_ ? setIsActive(false): setIsActive(item.uuid_)}}>view details</button></div>
+          <div className="grid-item1">
+            <Button text={isActive===item.uuid_? 'hide details':'view details'} onClick={()=>{isActive===item.uuid_ ? setIsActive(false): setIsActive(item.uuid_)}} />
+          </div>
           </div>
           <div className="grid-item1 grid-container2 btr" id={item.uuid_} style={{display: isActive===item.uuid_ ? 'grid' : 'none',}}><div className='grid-item1 grid-container2'><h4>Description</h4><p id="left">{item.description}</p>
           </div>
